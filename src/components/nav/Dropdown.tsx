4f9f1ce22568fdec555aad112e78dcd874e6de17
이호연 <ho991217@kakao.com>
2022-05-27 19:17:09 +0900

    feat: 애니메이션 추가

--- src/components/nav/Dropdown.tsx
+++ src/components/nav/Dropdown.tsx
@@ -8,6 +8,46 @@ const Ul = styled.ul`
   width: 150px;
   background-color: ${(props) => props.theme.colors.primary};
   z-index: 15;
+
+  :last-child {
+    border-radius: 0px 0px 5px 5px;
+  }
+
+  -webkit-animation: swing-in-top-fwd 0.5s
+    cubic-bezier(0.175, 0.885, 0.32, 1.275) both;
+  animation: swing-in-top-fwd 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275) both;
+  @-webkit-keyframes swing-in-top-fwd {
+    0% {
+      -webkit-transform: rotateX(-100deg);
+      transform: rotateX(-100deg);
+      -webkit-transform-origin: top;
+      transform-origin: top;
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: rotateX(0deg);
+      transform: rotateX(0deg);
+      -webkit-transform-origin: top;
+      transform-origin: top;
+      opacity: 1;
+    }
+  }
+  @keyframes swing-in-top-fwd {
+    0% {
+      -webkit-transform: rotateX(-100deg);
+      transform: rotateX(-100deg);
+      -webkit-transform-origin: top;
+      transform-origin: top;
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: rotateX(0deg);
+      transform: rotateX(0deg);
+      -webkit-transform-origin: top;
+      transform-origin: top;
+      opacity: 1;
+    }
+  }
 `;
 
 const Li = styled.li`
@@ -25,7 +65,7 @@ const SubMenuLink = styled(Link)`
   color: ${(props) => props.theme.colors.white};
   font-weight: 600;
   font-size: 1.125rem;
-  display: flex; 
+  display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
