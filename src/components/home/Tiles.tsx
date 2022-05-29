44c0be3ba947deba05cd6d724a1e1ad896235310
이호연 <ho991217@kakao.com>
2022-05-29 23:55:28 +0900

    feat: tile 레이아웃 완성

--- src/components/home/Tiles.tsx
+++ src/components/home/Tiles.tsx
@@ -3,18 +3,37 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 950px;
   height: 574px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 40px;
 `;
 
-const BigTile = styled.div``;
+const BigTile = styled.div`
+  height: 100%;
+  width: 465px;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
 
-const SmallTileContainer = styled.div``;
+const SmallTileContainer = styled.div`
+  height: 100%;
+  width: 465px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-const SmallTile = styled.div``;
+const SmallTile = styled.div`
+  width: 100%;
+  height: 178px;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
 
 function Tiles(): JSX.Element {
   return (
     <Container>
-      <BigTile>Calendar</BigTile>
+      <BigTile>학사일정</BigTile>
       <SmallTileContainer>
         <SmallTile>총학소식</SmallTile>
         <SmallTile>실시간 인기청원</SmallTile>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,7 @@ import Tiles from 'components/home/Tiles';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  height: 100vh;
+  /* height: 100vh; */
   width: 100vw;
   display: flex;
   flex-direction: column;
