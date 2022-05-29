53448a21511336def8c69317d6aa1892bfedfbe1
이호연 <ho991217@kakao.com>
2022-05-29 19:53:35 +0900

    fix: 깃 머지 과정중 생긴 버그 해결

--- src/components/nav/Dropdown.tsx
+++ src/components/nav/Dropdown.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { NavigationProps } from './NavItems';
+import { NavigationProps } from './NavigationProps';
 
 const Ul = styled.ul`
   position: absolute;
