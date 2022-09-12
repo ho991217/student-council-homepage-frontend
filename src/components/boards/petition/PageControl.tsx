5547903acfd110ba9169d58282457f2b97a09e99
이호연 <ho991217@kakao.com>
2022-09-12 13:37:23 +0900

    chore: 페이징 수정

--- src/components/boards/petition/PageControl.tsx
+++ src/components/boards/petition/PageControl.tsx
@@ -71,14 +71,9 @@ function PageControl({
         </Link>
       )}
       <Indexes>
-        {Array.from({ length: 10 }, (_, i) => (
-          <Index
-            cur={i + 1 + Math.floor(currentPage / 10) === currentPage}
-            key={i}
-          >
-            <Link to={generateParams(i + 1 + Math.floor(currentPage / 10))}>
-              {i + 1 + Math.floor(currentPage / 10)}
-            </Link>
+        {Array.from({ length: pageCount }, (_, i) => (
+          <Index cur={i + 1 === currentPage} key={i}>
+            <Link to={generateParams(i + 1)}>{i + 1}</Link>
           </Index>
         ))}
       </Indexes>
