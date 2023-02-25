45c70106b076cb4b2bc72cfa185ba3af7b2f91bb
이호연 <ho991217@kakao.com>
2023-02-25 14:00:35 +0900

    chore: gnb 수정

--- src/pages/council-info/Greeting.tsx
+++ src/pages/council-info/Greeting.tsx
@@ -2,7 +2,7 @@ import Block from 'components/Block';
 import styled from 'styled-components';
 
 // TODO: 이미지 스토리지 서버에 올라가면 변경하기
-import samplePhoto from 'static/images/samples/council-image.jpg';
+import samplePhoto from 'static/images/samples/council-image.png';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -27,16 +27,22 @@ function Greeting(): JSX.Element {
         contents={
           <div>
             <P style={{ lineHeight: 1.4 }}>
-              안녕하십니까
+              감사합니다.
               <br />
-              단국대학교 죽전캠퍼스 학우 여러분, 54대 PLAY! 총학생회입니다:)
               <br />
-              <br /> 학우 여러분에게 2022년은 어떤 해가 되고있나요? 저희 PLAY!
-              총학생회는 학우 여러분들의 2022년을 보다 알차고 낭만 가득하게
-              채워드리기 위해 다양한 정책을 준비하여 시행 중에 있습니다.
-              <br /> 학우 여러분들이 2022년의 단국을 행복으로 기억할 수 있도록
-              PLAY! 총학생회가 항상 학우 여러분과 소통하고 발전해
-              나아가겠습니다.
+              그대의 청춘에 단국을 담다,
+              <br />
+              <br />
+              안녕하십니까 단국대학교 죽전 캠퍼스 학우 여러분, 55대 담다
+              총학생회입니다.
+              <br />
+              <br />
+              2023년, 우리 단국은 코로나19로부터 벗어나 제약 없는 대면
+              학교생활을 맞이하게됩니다.
+              <br />
+              저희 담다 총학생회는 학우 여러분의 청춘의 한 페이지에 단국을 담을
+              수 있도록, 학우 여러분의 다양한 목소리를 담아 더 나은 학교를
+              만들기 위해 노력하겠습니다.
               <br />
               <br />
               감사합니다.
