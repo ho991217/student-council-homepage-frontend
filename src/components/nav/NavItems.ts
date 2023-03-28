826fdb62f1a78f2c63ecf5393a3c46fa418498ed
gaeunnlee <gaeunnnleee@gmail.com>
2023-03-28 10:10:59 +0900

    chore: 네비게이션 수정

--- src/components/nav/NavItems.ts
+++ src/components/nav/NavItems.ts
@@ -2,28 +2,11 @@ import { NavigationProps } from './NavigationProps';
 
 // TODO: 각 페이지 만들어지면 경로 수정v
 export const NavItems: NavigationProps[] = [
-  {
-    title: '총학소식',
-    path: '/',
-    id: 1,
-    subPath: [
-      {
-        title: '총학소식',
-        path: '/council-news',
-        id: 1,
-      },
-      {
-        title: '공약',
-        path: '/pledge',
-        id: 2,
-      },
-    ],
-  },
 
   {
-    title: '총학생회 정보',
+    title: '총학생회',
     path: '/',
-    id: 2,
+    id: 1,
     subPath: [
       {
         title: '인사말',
@@ -31,14 +14,36 @@ export const NavItems: NavigationProps[] = [
         id: 1,
       },
       {
+        title: '공약',
+        path: '/pledge',
+        id: 2,
+      },
+      {
         title: '조직도',
         path: '/organization',
-        id: 2,
+        id: 3,
       },
       {
         title: '오시는길',
         path: '/location',
-        id: 3,
+        id: 4,
+      },
+    ],
+  },
+  {
+    title: '총학소식',
+    path: '/',
+    id: 2,
+    subPath: [
+      {
+        title: '총학소식',
+        path: '/council-news',
+        id: 1,
+      },
+      {
+        title: '회칙',
+        path: '/rules',
+        id: 2,
       },
     ],
   },
@@ -63,7 +68,7 @@ export const NavItems: NavigationProps[] = [
         id: 1,
       },
       {
-        title: '문의 및 건의사항',
+        title: '자유게시판',
         path: '/board-suggestion/boards?page=1',
         id: 2,
       },
