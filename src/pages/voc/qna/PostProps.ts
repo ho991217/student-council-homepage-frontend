de7d546637871ccee784bdc21103ab95617e6ec4
seungki <sysh9498@gmail.com>
2023-04-04 00:48:31 +0900

    docs: 게시물 Props 설정

---
+++ src/pages/voc/qna/PostProps.ts
@@ -0,0 +1,12 @@
+export interface PostProps {
+  commentCount: number;
+  id: number;
+  views: number;
+  title: string;
+  category: string;
+  status: string;
+  likes: number;
+  comments: number;
+  createdAt: string;
+  author: string;
+}
