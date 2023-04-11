82f602f863e58bced3612155f025975d682800fd
박찬진 <68864422+developerChans@users.noreply.github.com>
2023-04-11 21:37:04 +0900

    fix: bug/251 신청현황변경

--- src/pages/rental/RentalInfo.tsx
+++ src/pages/rental/RentalInfo.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
 import qs from 'qs';
 import { useEffect, useState } from 'react';
-import { BoardComponents, Container } from './components/Board.style';
+import PageControl, { PagingProps } from 'components/PageControl';
+import { BoardComponents, Container } from './components/InfoBoard.style';
 import { H1, H2, Hr } from './RentalLists';
 
 const HeaderContainer = styled.div`
@@ -16,31 +18,91 @@ const HeaderContainer = styled.div`
   margin: 20px 0 32px 0;
   border-radius: 15px;
 `;
+export type RentalInfoList = {
+  id: number;
+  name: string;
+  remaining: number;
+}[];
 
 function RentalInfo() {
   const [searchParams] = useSearchParams();
-  const [productInfo, setProductInfo] = useState({ id: 0, name: 'no-infos' });
+  const [productInfo, setProductInfo] = useState<RentalInfoList>([]);
+  const [page, setPage] = useState<number>(1);
+  const [pagingInfo, setPagingInfo] = useState<PagingProps>({
+    hasNext: false,
+    totalPages: 0,
+    totalElements: 0,
+    page: 0,
+    size: 0,
+    first: true,
+    last: false,
+  });
+
+  const[itemName, setitemName] = useState({name: ''});
+
+  const getRentalInfoLists = async (id: number) => {
+    let { page } = qs.parse(searchParams.toString());
+    
+    if (!page) page = '1';
+    const {
+      data: { content, ...options },
+    } = await axios({
+      method: 'get',
+      url: `/rental/item?sort=name&page=${Number(page) - 1}&size=6`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setProductInfo(content);
+    setPagingInfo(options);
+  };
 
   useEffect(() => {
-    const { id, name } = qs.parse(searchParams.toString());
+    const { id } = qs.parse(searchParams.toString());
+    getRentalInfoLists(Number(id));
+  }, [page]);
 
-    if (typeof name === 'string') setProductInfo({ id: Number(id), name });
+  useEffect(() => {
+    // const { id, name } = qs.parse(searchParams.toString());
+    const { id } = qs.parse(searchParams.toString());
+
+    setPage(Number(searchParams.get('page')) || 1);
   }, [searchParams]);
+
+  useEffect(() => {
+    // const { id, name } = qs.parse(searchParams.toString());
+    const { name } = qs.parse(searchParams.toString());
+
+
+    if (typeof name === 'string') setitemName({ name });
+  }, [searchParams]);  
 
   return (
     <Container>
       <HeaderContainer>
         <Hr />
-        <H1>{productInfo.name}</H1>
+        <H1>{itemName.name}</H1>
         <H2>대여 물품 현황 페이지 입니다.</H2>
       </HeaderContainer>
       <BoardComponents.BoardContainer>
         <BoardComponents.Index>
-          <BoardComponents.Col>신청일자</BoardComponents.Col>
+          <BoardComponents.Col>행사명</BoardComponents.Col>
           <BoardComponents.Col>사용일시</BoardComponents.Col>
+          <BoardComponents.Col>사용자구분</BoardComponents.Col>
           <BoardComponents.Col>사용자</BoardComponents.Col>
         </BoardComponents.Index>
+
+        {productInfo.map((el) => (
+          <BoardComponents.Item key={el.id}>
+          <BoardComponents.Col>{el.id}</BoardComponents.Col>
+          <BoardComponents.Col>{el.name}</BoardComponents.Col>
+          <BoardComponents.Col>{el.remaining}</BoardComponents.Col>
+          <BoardComponents.Col>{el.remaining}</BoardComponents.Col>
+        </BoardComponents.Item>
+        ))}
       </BoardComponents.BoardContainer>
+      <PageControl currentPage={page} pagingInfo={pagingInfo} />
+
     </Container>
   );
 }
