44c367f861af9be38e9a2ae3219deea488f2c5ba
seungki <sysh9498@gmail.com>
2023-04-12 00:30:38 +0900

    fix: 게시판 margin과 padding 수정

--- src/pages/voc/my-voice/components/Board.tsx
+++ src/pages/voc/my-voice/components/Board.tsx
@@ -18,10 +18,10 @@ const Wrapper = styled.div`
   max-width: 1290px;
   width: 100%;
   ${({ theme }) => theme.media.desktop} {
-    padding: 10px 50px 10px 0px;
+    padding: 10px 0px 10px 0px;
   }
   ${({ theme }) => theme.media.tablet} {
-    padding: 30px 50px 10px 0px;
+    padding: 30px 50px 10px 50px;
   }
   ${({ theme }) => theme.media.mobile} {
     padding: 30px 10px 20px 10px;
@@ -39,7 +39,7 @@ const BoardsContainer = styled.div`
 const BoardHead = styled.div`
   width: 100%;
   height: 70px;
-  border-top: 3px solid #434343;
+  border-top: 3px solid ${({ theme }) => theme.colors.gray100};
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray100};
 `;
 
@@ -104,10 +104,8 @@ function Board({ posts, pagingInfo, currentPage }: BoardProps): JSX.Element {
               </BoardHead>
               {board.map((post, index) => (
                 <Row key={post.id}>
-                  <div>
-                    {index + 1 + pagingInfo.page * pagingInfo.size}
-                  </div>
-                  <div>{post.createdAt}</div>
+                  <div>{index + 1 + pagingInfo.page * pagingInfo.size}</div>
+                  <div>{post.createdAt.substring(0, 10)}</div>
                   <LinkDiv>
                     {post.status === '정지' ? (
                       <Link
@@ -143,10 +141,8 @@ function Board({ posts, pagingInfo, currentPage }: BoardProps): JSX.Element {
               </BoardHead>
               {board.map((post, index) => (
                 <Row key={post.id}>
-                  <div>
-                    {index + 1 + pagingInfo.page * pagingInfo.size}
-                  </div>
-                  <div>{post.createdAt}</div>
+                  <div>{index + 1 + pagingInfo.page * pagingInfo.size}</div>
+                  <div>{post.createdAt.substring(0, 10)}</div>
                   <LinkDiv>
                     {post.status === '정지' ? (
                       <Link
@@ -181,11 +177,9 @@ function Board({ posts, pagingInfo, currentPage }: BoardProps): JSX.Element {
               </BoardHead>
               {board.map((post, index) => (
                 <Row key={post.id}>
-                  <div>
-                    {index + 1 + pagingInfo.page * pagingInfo.size}
-                  </div>
+                  <div>{index + 1 + pagingInfo.page * pagingInfo.size}</div>
 
-                  <div>{post.createdAt}</div>
+                  <div>{post.createdAt.substring(0, 10)}</div>
                   <LinkDiv>
                     {post.status === '정지' ? (
                       <Link
