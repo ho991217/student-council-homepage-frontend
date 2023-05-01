6d45cc703a6a1a1e54b4e00ac663c1c040809a95
gaeunnlee <gaeunnnleee@gmail.com>
2023-05-01 18:21:30 +0900

    bug: 새로고침시 메인페이지로 이동하는 오류 해결

--- src/AuthRoute.tsx
+++ src/AuthRoute.tsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 function AuthRoute({ children }: { children: JSX.Element }) {
   const { isLogin } = useLogin();
 
-  return isLogin() ? children : <Navigate to="/login" />;
+  return isLogin() ? children : <div>로그인해주세요</div>;
 }
 
 export default AuthRoute;
